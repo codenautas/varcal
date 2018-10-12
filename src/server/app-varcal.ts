fb0788d97c8500ea481c84886680113a6e29cace
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-10-12 11:42:49 -0300

    some refactors; manually adds prefixes for operativos where appropiate

--- src/server/app-varcal.ts
+++ src/server/app-varcal.ts
@@ -5,8 +5,8 @@ import * as operativos from "operativos";
 import { procedures } from "./procedures-varcal";
 import { alias } from "./table-alias";
 import { Client } from "pg-promise-strict";
-import { TablaDatos, UnidadDeAnalisis, Request, tiposTablaDato, Operativo } from "operativos";
-import { generateConditions, sufijo_agregacion } from "./var-cal";
+import { TablaDatos, UnidadDeAnalisis, Request, tiposTablaDato, Operativo, TablaDatosDB } from "operativos";
+import { buildONClausule, sufijo_agregacion } from "./var-cal";
 import { AliasDefEst, DefinicionEstructural, DefinicionEstructuralTabla} from "./types-varcal";
 
 // re-export my file of types for external modules
@@ -26,33 +26,34 @@ export function emergeAppVarCal<T extends Constructor<operativos.AppOperativosTy
         }
 
         async postConfig(){
+            await super.postConfig();
             var be=this;
             await be.inTransaction({} as Request, async function(client:Client){
                 let operativos = await client.query('SELECT * from operativos').fetchAll();
                 await Promise.all(operativos.rows.map((ope: Operativo) => be.armarDefEstructural(client, ope.operativo)));
             });
-            await super.postConfig();
         }
 
-        async generateBaseTableDef(client: Client, tablaDatos:TablaDatos){
-            let td = await super.generateBaseTableDef(client, tablaDatos);
+        async generateBaseTableDef(client: Client, tablaDatosDB:TablaDatosDB){
+            let tablaDatos = TablaDatos.construirConObj(tablaDatosDB);
+            let tDef = await super.generateBaseTableDef(client, tablaDatos);
             //TODO: dejar de preguntar por el postfix agregar un campo "esCalculada" a tablaDatos 
-            if (tablaDatos.tipo == tiposTablaDato.calculada){
+            if (tablaDatos.esCalculada()){
                 let estParaGenTabla:DefinicionEstructuralTabla = this.defEsts[tablaDatos.operativo].tables[tablaDatos.unidad_analisis];
-                td.foreignKeys = [{ references: estParaGenTabla.sourceBro, fields: estParaGenTabla.pks, onDelete: 'cascade', displayAllFields: true }];
-                td.detailTables = estParaGenTabla.detailTables;
-                td.sql.isReferable = true;
+                // esto se agrega para que las calculadas muestren también todos los campos de su sourceBro
+                tDef.foreignKeys = [{ references: estParaGenTabla.sourceBro, fields: estParaGenTabla.pks, onDelete: 'cascade', displayAllFields: true }];
+                tDef.detailTables = estParaGenTabla.detailTables;
+                tDef.sql.isReferable = true;
             }
-            return td
+            return tDef
         }
 
-        sufijarUACalculada(ua:string){
+        static sufijarCalculada(str:string){
             // sufija la UA Calculada
-            return ua + '_' + tiposTablaDato.calculada;
+            return str + '_' + tiposTablaDato.calculada;
         }
 
         async armarDefEstructural(client: Client, operativo: string){
-            let be:AppVarCal = this;
             var sqlParams=[operativo];
             var results={
                 aliases: await client.query(
@@ -63,7 +64,7 @@ export function emergeAppVarCal<T extends Constructor<operativos.AppOperativosTy
                 ).fetchAll(),
                 tables: await client.query(
                     `SELECT ua.*, to_jsonb(array_agg(v.variable order by v.orden)) pk_arr
-                    FROM unidad_analisis ua join variables v on v.operativo=ua.operativo and v.unidad_analisis=ua.unidad_analisis and v.es_pk
+                    FROM unidad_analisis ua JOIN tabla_datos td ON ua.operativo = td.operativo AND ua.unidad_analisis = td.unidad_analisis JOIN variables v on v.operativo=ua.operativo and v.tabla_datos=td.tabla_datos and v.es_pk
                     where ua.operativo=$1
                     group by ua.operativo, ua.unidad_analisis`
                     , sqlParams
@@ -75,21 +76,22 @@ export function emergeAppVarCal<T extends Constructor<operativos.AppOperativosTy
             }
             results.aliases.rows.forEach(function(a:{alias:string, alias_def_est:AliasDefEst}){ defEst.aliases[a.alias]=a.alias_def_est });
             //falta trabajar results para obtener la pinta de  defEst
-            results.tables.rows.forEach(function(table: UnidadDeAnalisis & {pk_arr: string[]}){
-                let tua = table.unidad_analisis;
+            results.tables.rows.forEach(function(table: UnidadDeAnalisis & TablaDatos & {pk_arr: string[]}){
+                let tua = table.operativo.toLowerCase() + '_' + table.unidad_analisis;
                 let tDefEst:DefinicionEstructuralTabla = {
-                    sourceBro : tua,
-                    target: tua + '_' + tiposTablaDato.calculada,
+                    operativo: operativo,
+                    sourceBro :  tua,
+                    target: AppVarCal.sufijarCalculada(tua),                    
                     pks : table.pk_arr,
                     aliasAgg : tua + sufijo_agregacion,
                 }
-                tDefEst.where = generateConditions(tDefEst.sourceBro, tDefEst.target, table.pk_arr);
+                tDefEst.where = buildONClausule(tDefEst.sourceBro, tDefEst.target, table.pk_arr);
                 
                 tDefEst.whereAgg = {};
                 tDefEst.sourceJoin = '';
                 if (table.padre){
-                    tDefEst.sourceAgg = tDefEst.target + ` inner join ${tua} ON ` + generateConditions(tDefEst.target, tua, table.pk_arr);
-
+                    tDefEst.sourceAgg = tDefEst.target + ` inner join ${tDefEst.sourceBro} ON ` + buildONClausule(tDefEst.target, tDefEst.sourceBro, table.pk_arr);
+                    let padrePrefijado = table.operativo.toLowerCase() + '_' + table.padre;
                     //calculo pks del padre sacando de la lista completa de pks las agregadas por esta tabla hija
                     let pksPadre: string[] = table.pk_arr.slice(); //copia por valor para no modificar la lista de pks completa
                     table.pk_agregada.split(',').forEach(pkAgregada => {
@@ -99,15 +101,15 @@ export function emergeAppVarCal<T extends Constructor<operativos.AppOperativosTy
                         }
                     });
                     // Calculo whereAgg
-                    tDefEst.whereAgg[table.padre] = generateConditions(table.padre, tDefEst.target, pksPadre);
+                    tDefEst.whereAgg[table.padre] = buildONClausule(padrePrefijado, tDefEst.target, pksPadre);
                     // Calculo sourceJoin
-                    tDefEst.sourceJoin = `inner join ${table.padre} using (${pksPadre.join(', ')})`;
+                    tDefEst.sourceJoin = `inner join ${padrePrefijado} using (${pksPadre.join(', ')})`;
                 }else {
                     tDefEst.sourceAgg = tDefEst.target;
                 }
                 tDefEst.detailTables= [];
                 
-                defEst.tables[tua] = tDefEst;
+                defEst.tables[table.unidad_analisis] = tDefEst;
             });
             
             //Seteo de detail tables a los padres de las tablas que tienen padre, se hace por separado para que todos los padres ya esten completos
@@ -119,7 +121,7 @@ export function emergeAppVarCal<T extends Constructor<operativos.AppOperativosTy
                     abr: table.unidad_analisis.substr(0,1).toUpperCase()
                 });
                 defEst.tables[table.padre].detailTables.push({
-                    table: be.sufijarUACalculada(table.unidad_analisis),
+                    table: AppVarCal.sufijarCalculada(table.unidad_analisis),
                     fields: defEst.tables[table.padre].pks,
                     abr: table.unidad_analisis.substr(0,1).toUpperCase() + '-C'
                 });
