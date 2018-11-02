10a0875a82841a699e60bdb54e20253629605c9d
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-11-02 18:32:47 -0300

    Se modulariza documenta y simplifica el código siempre respetando tests

--- src/server/var-cal.ts
+++ src/server/var-cal.ts
@@ -22,7 +22,7 @@ export function getAggregacion(f: string, exp: string) {
 }
 
 
-export function buildWhereConditions(leftAlias:string, rigthAlias:string, columnsToJoin: string[]){
+export function buildWhereConditions(leftAlias: string, rigthAlias: string, columnsToJoin: string[]) {
     return columnsToJoin.map((col: string) =>
         `${leftAlias}.${col} = ${rigthAlias}.${col}`
     ).join(' and ');
@@ -33,12 +33,12 @@ export function getInsumos(expression: string): ExpresionParser.Insumos {
     return ExpresionParser.parse(expression).getInsumos();
 }
 
-export function getWrappedExpression(expression: string|number, pkExpression: string, options: ExpresionParser.CompilerOptions): string {
+export function getWrappedExpression(expression: string | number, pkExpression: string, options: ExpresionParser.CompilerOptions): string {
     var compiler = new ExpresionParser.Compiler(options);
     return compiler.toCode(ExpresionParser.parse(expression), pkExpression);
 }
 
-export const sufijo_agregacion:string='_agg';
+export const sufijo_agregacion: string = '_agg';
 
 
 
@@ -48,19 +48,19 @@ export const sufijo_agregacion:string='_agg';
 // Se reactiva codigo antiguo para de separarGruposPorOirgenYNivel 
 // para testear > debuguear > documentar > modularizar > refactorizar > llevar al modelo lo que sirva
 
-export interface AliasDefEst{
+export interface AliasDefEst {
     on: string
     tabla_datos: string
     where?: string
 }
 
-export interface Alias extends AliasDefEst{
+export interface Alias extends AliasDefEst {
     operativo: string
     alias: string
     descripcion?: string
 }
 
-export type PrefixedPks = {[key:string]: {pks:string[], pksString: string}};
+export type PrefixedPks = { [key: string]: { pks: string[], pksString: string } };
 
 export interface Join {
     tabla: string,
@@ -88,23 +88,23 @@ export class BloqueVariablesACalcular {
     tabla: string
     variables: VariableCalculada[]
 
-    constructor(vCalc:VariableCalculada){
-        this.tabla= vCalc.tabla_datos;
-        this.variables=[vCalc];
+    constructor(vCalc: VariableCalculada) {
+        this.tabla = vCalc.tabla_datos;
+        this.variables = [vCalc];
     }
 };
 export type DefinicionEstructuralTabla = {
     operativo?: string;
     target?: string;
     sourceBro?: string;
-    pks?:string[];
+    pks?: string[];
     sourceJoin?: string;
     where?: string;
     aliasAgg?: string;
     sourceAgg?: string;
-    whereAgg?:{ 
+    whereAgg?: {
         [key: string]: string
-    }    
+    }
 };
 
 export type Tables = {
@@ -120,30 +120,38 @@ export interface Aliases {
     [key: string]: AliasDefEst
 }
 
-function hasTablePrefix(variable: string){
+function hasTablePrefix(variable: string) {
     return variable.match(/^.+\..+$/);
 }
 
-function checkInsumos(vCalc: VariableCalculada, definedVars: string[], definicionesOrd: VariableCalculada[], nonDefinedVars: VariableCalculada[], defEst: DefinicionEstructural): boolean {
-    var cantDef: number = 0;
-    vCalc.insumos.variables.forEach(function (varInsumosName) {
-        // si esta variable tiene un prefijo && la variable sin prefijo está definida && el prefijo está en la tabla de aliases
-        if (hasTablePrefix(varInsumosName) && defEst) {
-            var [prefix, varName] = varInsumosName.split('.');
-            if (definedVars.indexOf(varName) > -1 && (prefix in { ...defEst.tables, ...defEst.aliases })) {
-                definedVars.push(varInsumosName);// then agrego esta variable a definedVars
-            }
-        }
-        cantDef = definedVars.indexOf(varInsumosName) >= 0 ? cantDef + 1 : cantDef;
-    });
-    if (cantDef == vCalc.insumos.variables.length) {
-        definedVars.push(vCalc.variable);
-        definicionesOrd.push(vCalc);
-        if (nonDefinedVars.indexOf(vCalc) >= 0) {
-            nonDefinedVars.splice(nonDefinedVars.indexOf(vCalc), 1)
+function checkInsumos(vCalc: VariableCalculada, definedVars: string[], defEst: DefinicionEstructural): boolean {
+    var countChecked: number = 0;
+    vCalc.insumos.variables.forEach(
+        varInsumos => countChecked = checkAndPushVar(varInsumos, definedVars, defEst)? countChecked + 1 : countChecked);
+    // están todas las insumosVars definidas si se cumple la igualdad
+    return countChecked == vCalc.insumos.variables.length;
+}
+
+function checkAndPushVar(varInsumosName: string, definedVars: string[], defEst: DefinicionEstructural,) {
+    let varChecked = definedVars.indexOf(varInsumosName) >= 0;
+    if (!varChecked && insumoVarIsWellPrefixed(varInsumosName, definedVars, defEst)) {
+        definedVars.push(varInsumosName);
+        varChecked = true;
+    }
+    // si la variable de insumo estaba definida previamente o porque se acaba de agregar al array 
+    return varChecked;
+}
+
+function insumoVarIsWellPrefixed(varInsumosName: string, definedVars: string[], defEst: DefinicionEstructural) {
+    // si esta variable tiene un prefijo && la variable sin prefijo está definida && el prefijo está en la tabla de aliases
+    let isDefined = false;
+    if (hasTablePrefix(varInsumosName) && defEst) {
+        var [prefix, varName] = varInsumosName.split('.')
+        if (definedVars.indexOf(varName) > -1 && (prefix in { ...defEst.tables, ...defEst.aliases })) {
+            isDefined = true
         }
     }
-    return cantDef == vCalc.insumos.variables.length;
+    return isDefined;
 }
 
 /**
@@ -151,44 +159,38 @@ function checkInsumos(vCalc: VariableCalculada, definedVars: string[], definicio
  * @param definedVars variables con insumos definidos
  */
 export function separarEnGruposPorNivelYOrigen(nonDefinedVars: VariableCalculada[], definedVars: string[], defEst?: DefinicionEstructural): BloqueVariablesACalcular[] {
-    var listaOut: BloqueVariablesACalcular[] = [];
-    var lenAnt: number;
-    var definicionesOrd: VariableCalculada[] = [];
-    do {
-        lenAnt = nonDefinedVars.length;
-        var i = 0;
-        while (i < nonDefinedVars.length) {
-            if (!checkInsumos(nonDefinedVars[i], definedVars, definicionesOrd, nonDefinedVars, defEst)) {
-                i++;
-            }
-        };
-    } while (nonDefinedVars.length > 0 && nonDefinedVars.length != lenAnt);
-    if (nonDefinedVars.length > 0) {
-        throw new Error("Error, no se pudo determinar el orden de la variable '" + nonDefinedVars[0].variable + "' y otras")
-    }
-    definicionesOrd.forEach(function (vCalc: VariableCalculada) {
+    var orderedCalcVars: VariableCalculada[] = sortCalcVariablesByDependency(nonDefinedVars, definedVars, defEst);
+    return separarEnGrupos(orderedCalcVars);
+}
+
+function separarEnGrupos(orderedCalcVars: VariableCalculada[]) {
+    let listaOut: BloqueVariablesACalcular[] = [];
+    orderedCalcVars.forEach(function (vCalc: VariableCalculada) {
         if (listaOut.length == 0) {
             listaOut.push(new BloqueVariablesACalcular(vCalc));
-        } else {
+        }
+        else {
             var enNivel = vCalc.insumos.variables.length ? vCalc.insumos.variables.map(function (varInsumo) {
                 return listaOut.findIndex(function (nivel) {
                     return nivel.variables.findIndex(function (vvar) {
-                        return vvar.variable == varInsumo
-                    }) == -1 ? false : true
-                })
+                        return vvar.variable == varInsumo;
+                    }) == -1 ? false : true;
+                });
             }).reduce(function (elem: number, anterior: number) {
                 return elem > anterior ? elem : anterior;
             }) : 0;
             if (enNivel >= 0 && listaOut.length === enNivel + 1) {
                 listaOut.push(new BloqueVariablesACalcular(vCalc));
-            } else {
+            }
+            else {
                 let tabla = vCalc.tabla_datos;
                 var nivelTabla = listaOut[enNivel + 1].tabla == tabla ? enNivel + 1 : listaOut.findIndex(function (nivel, i) {
-                    return nivel.tabla == tabla && i > enNivel + 1
+                    return nivel.tabla == tabla && i > enNivel + 1;
                 });
                 if (nivelTabla >= 0) {
                     listaOut[nivelTabla].variables.push(vCalc);
-                } else {
+                }
+                else {
                     listaOut.push(new BloqueVariablesACalcular(vCalc));
                 }
             }
@@ -196,3 +198,33 @@ export function separarEnGruposPorNivelYOrigen(nonDefinedVars: VariableCalculada
     });
     return listaOut;
 }
+
+function sortCalcVariablesByDependency(nonDefinedVars: VariableCalculada[], definedVars: string[], defEst: DefinicionEstructural) {
+    var orderedCalcVars: VariableCalculada[] = [];
+    var prevNonDefVarsLength: number;
+    do {
+        prevNonDefVarsLength = nonDefinedVars.length;
+        findNewDefinedVars(nonDefinedVars, definedVars, defEst, orderedCalcVars);
+    } while (nonDefinedVars.length > 0 && nonDefinedVars.length != prevNonDefVarsLength);
+    if (nonDefinedVars.length > 0) {
+        throw new Error("Error, no se pudo determinar el orden de la variable '" + nonDefinedVars[0].variable + "' y otras");
+    }
+    return orderedCalcVars;
+}
+function findNewDefinedVars(nonDefinedVars: VariableCalculada[], definedVars: string[], defEst: DefinicionEstructural, orderedCalcVars: VariableCalculada[]) {
+    var i = 0;
+    while (i < nonDefinedVars.length) {
+        let vCalc = nonDefinedVars[i];
+        // si todas las variables de insumo de una varCalc están definidas,
+        if (checkInsumos(vCalc, definedVars, defEst)) {
+            // entonces se agrega la variable en cuestión como definida y se elimina de la lista de no definidas
+            definedVars.push(vCalc.variable);
+            orderedCalcVars.push(vCalc);
+            nonDefinedVars.splice(i, 1);
+        }
+        else {
+            i++;
+        }
+    }
+}
+
