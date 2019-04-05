c8edef4ba1e159f5370b91e0082e67d2322fd501
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2019-04-05 13:48:57 -0300

    in the future this class will be in charge of parse expression, validate all insumos, and build the estructures for future use in sql generation (orderedInsumosTDs, etc)

---
+++ src/server/complex-expression.ts
@@ -0,0 +1,56 @@
+import * as EP from "expre-parser";
+import { Relacion, TablaDatos} from "operativos";
+// import { BaseNode, Insumos } from "expre-parser";
+
+export class ComplexExpression{
+    // private validateOverwritingNames(insumos: Insumos): void {
+    //     if (insumos.funciones) {
+    //         insumos.variables.forEach(varName => {
+    //             if (insumos.funciones.indexOf(varName) > -1) {
+    //                 throw new Error('La variable "' + varName + '" es también un nombre de función');
+    //             }
+    //         })
+    //     }
+    // }
+    // private validateFunctions(funcNames: string[]) {
+    //     let pgWitheList = ['div', 'avg', 'count', 'max', 'min', 'sum', 'coalesce'];
+    //     let comunSquemaWhiteList = ['informado'];
+    //     let functionWhiteList = pgWitheList.concat(comunSquemaWhiteList);
+    //     funcNames.forEach(f => {
+    //         if (hasAlias(f)) {
+    //             if (f.split('.')[0] != 'dbo') {
+    //                 throw new Error('La Función ' + f + ' contiene un alias inválido');
+    //             }
+    //         } else {
+    //             if (functionWhiteList.indexOf(f) == -1) {
+    //                 throw new Error('La Función ' + f + ' no está incluida en la whiteList de funciones: ' + functionWhiteList.toString());
+    //             }
+    //         }
+    //     })
+    // }
+    // validateInsumos(): void {
+    //     this.validateOverwritingNames(this.insumos);
+    //     this.validateFunctions(this.insumos.funciones);
+    // }
+    tdsNeedByExpression: string[] = [];
+    
+    expresionProcesada!: string
+    insumos!: EP.Insumos; 
+    
+    orderedInsumosTDNames: string[] = []
+    insumosOptionalRelations: Relacion[] = [] 
+    lastTD!:TablaDatos
+    
+    clausula_from!:string
+    clausula_where!:string
+    
+    // constructor(public userExpression:string){
+    //     this.setInsumos()
+    //     this.validateInsumos()
+    // }
+    
+    // private setInsumos(){
+    //     let bn:BaseNode = EP.parse(this.userExpression); 
+    //     this.insumos = bn.getInsumos();
+    // }
+}
