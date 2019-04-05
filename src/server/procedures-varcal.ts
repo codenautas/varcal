8cf1866cebac334dddfd9f6ae84581c419968df0
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2019-04-05 11:39:55 -0300

    fix type

--- src/server/procedures-varcal.ts
+++ src/server/procedures-varcal.ts
@@ -3,6 +3,7 @@
 import * as fs from "fs-extra";
 import { AppVarCalType } from "./app-varcal";
 import { ProcedureContext, VarCalculator } from "./types-varcal";
+import { coreFunctionParameters } from "operativos";
 
 var procedures = [
     {
@@ -10,7 +11,7 @@ var procedures = [
         parameters: [
             { name: 'operativo', typeName: 'text', references: 'operativos', }
         ],
-        coreFunction: async function (context: ProcedureContext, parameters: {operativo: string}) {
+        coreFunction: async function (context: ProcedureContext, parameters: coreFunctionParameters) {
             let varCalculator = new VarCalculator(context.be as AppVarCalType, context.client, parameters.operativo);
             await varCalculator.fetchDataFromDB();
             let todoElScript:string = await varCalculator.calculate();
