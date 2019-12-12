7132893160cd0038e9b9209ec5b910a762723a67
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2019-12-12 15:14:05 -0300

    ilegal scaping in string template literals

--- src/server/var-calculator.ts
+++ src/server/var-calculator.ts
@@ -105,7 +105,7 @@ export class VarCalculator extends ExpressionProcessor {
           execute replace(regexp_replace(replace(
             v_sql,
             $$update_varcal_por_encuesta("p_operativo" text, "p_id_caso" text) RETURNS TEXT$$, $$update_varcal("p_operativo" text) RETURNS TEXT$$),
-            $$(.* )".*"\.${quoteIdent(OperativoGenerator.mainTDPK)}=p_id_caso(.*)$$, $$\1TRUE\2$$,'gm'),
+            $$(.* )".*"\\.${quoteIdent(OperativoGenerator.mainTDPK)}=p_id_caso(.*)$$, $$\\1TRUE\\2$$,'gm'),
             $$${quoteIdent(OperativoGenerator.mainTDPK)}=p_id_caso$$, $$TRUE$$);
           return '2GENERATED';
         end;
